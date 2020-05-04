ea890f3bafd8b7c76f55e1e180f70ce0d07c4562
RavanSA <58227337+RavanSA@users.noreply.github.com>
2020-05-04 23:47:01 +0300

    Add files via upload

---
+++ hw1.h
@@ -0,0 +1,23 @@
+#define INTEGER 1
+#define FLOAT 2
+#define CHAR 3
+#define STRING 4
+#define BOOLEAN 5
+#define IDENTIFIER 6
+#define TYPE 7
+#define OPERATOR 8
+#define EQUAL 9
+#define CONDITION 10
+#define LOOP 11
+#define COMMENT 12
+#define METHOD 13
+#define FUNCTION 14
+#define DOT 15
+#define OPEN_BRACKET 16
+#define CLOSE_BRACKET 17
+#define OPEN_CURLY 18
+#define CLOSE_CURLY 19
+#define SEMICOLON 20
+
+
+
